261c38ca2ff2f018148cdb4e5c6c5e0284ed8957
Robin Obůrka <robin.oburka@nic.cz>
2013-08-01 16:47:12 +0200

    Some more development tests.

--- src/dev/main.c
+++ src/dev/main.c
@@ -23,6 +23,8 @@ int write_random_and_read(struct atsha_handle *handle) {
 	fprintf(stderr, "Generate random number status: %s\n", atsha_error_name(status));
 	if (status == ATSHA_ERR_OK) {
 		fprintf(stderr, "Generated %zu bytes random number: \n", number.bytes); for (size_t i = 0; i < number.bytes; i++) { printf("%02X ", number.data[i]); } printf("\n");
+	} else {
+		return status;
 	}
 
 	//Write to slot
@@ -72,8 +74,9 @@ int hmac(struct atsha_handle *handle) {
 	if (status == ATSHA_ERR_OK) {
 		fprintf(stderr, "HMAC F is %zu bytes number: \n", digest.bytes); for (size_t i = 0; i < digest.bytes; i++) { printf("%02X ", digest.data[i]); } printf("\n");
 		free(digest.data);
-		free(number.data);
 	}
+
+	free(number.data);
 
 	return status;
 }
@@ -135,6 +138,60 @@ int main(int argc, char **argv) {
 	//SN
 	fprintf(stderr, "Serial number:\n");
 	atsha_big_int sn;
+	status = atsha_serial_number(handle, &sn);
+	fprintf(stderr, "Status: %s\n", atsha_error_name(status));
+	if (status == ATSHA_ERR_OK) {
+		fprintf(stderr, "SN contents %zu bytes number: ", sn.bytes); for (size_t i = 0; i < sn.bytes; i++) { printf("%02X ", sn.data[i]); } printf("\n");
+		free(sn.data);
+	}
+
+	atsha_close(handle);
+
+	fprintf(stderr, "SW Emulation\n============================================================\n\n");
+
+	handle = atsha_open_emulation("atsha204.sw");
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open config file.\n");
+		return 1;
+	}
+
+	// Get Revision
+	fprintf(stderr, "Get revision:\n");
+	rev = 0;
+	status = atsha_dev_rev(handle, &rev);
+	fprintf(stderr, "Status: %s\n", atsha_error_name(status));
+	fprintf(stderr, "Revision: %u\n", rev);
+
+	// Random number
+	fprintf(stderr, "Random number:\n");
+	status = atsha_random(handle, &number);
+	fprintf(stderr, "Status: %s\n", atsha_error_name(status));
+	if (status == ATSHA_ERR_OK) {
+		fprintf(stderr, "%zu bytes number: ", number.bytes); for (size_t i = 0; i < number.bytes; i++) { printf("%02X ", number.data[i]); } printf("\n");
+		free(number.data);
+	}
+
+	// Read slot
+	fprintf(stderr, "Read slot:\n");
+	status = atsha_slot_read(handle, &number2);
+	fprintf(stderr, "Status: %s\n", atsha_error_name(status));
+	if (status == ATSHA_ERR_OK) {
+		fprintf(stderr, "Slot contents %zu bytes number: ", number2.bytes); for (size_t i = 0; i < number2.bytes; i++) { printf("%02X ", number2.data[i]); } printf("\n");
+		free(number2.data);
+	}
+
+	write_random_and_read(handle);
+
+	fprintf(stderr, "Config read:\n");
+	config_word = 0;
+	status = atsha_slot_conf_read(handle, 8, &config_word);
+	fprintf(stderr, "Status: %s\n", atsha_error_name(status));
+	fprintf(stderr, "Slot config word: %04X\n", config_word);
+
+	hmac(handle);
+
+	//SN
+	fprintf(stderr, "Serial number:\n");
 	status = atsha_serial_number(handle, &sn);
 	fprintf(stderr, "Status: %s\n", atsha_error_name(status));
 	if (status == ATSHA_ERR_OK) {
