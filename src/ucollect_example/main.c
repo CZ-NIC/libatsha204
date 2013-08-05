da722b3910731da1400eebea35265e5f17bcef69
Robin Obůrka <robin.oburka@nic.cz>
2013-08-05 16:29:19 +0200

    New lib implementation: Ucollect example

---
+++ src/ucollect_example/main.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdint.h>
+#include<stdbool.h>
+
+#include "../libatsha204/atsha204.h"
+
+void log_callback(const char *msg) {
+	fprintf(stderr, "Log: %s\n", msg);
+}
+
+unsigned char *get_random_number() {
+	unsigned char *random_number;
+	//OK, it is very dummy implementation
+	unsigned char data[32] = {
+		0x45, 0x78, 0xA1, 0xAB, 0xA4, 0x35, 0xB0, 0x93,
+		0x2F, 0x9F, 0x8A, 0xB4, 0x55, 0x9C, 0xA1, 0x1A,
+		0x13, 0x6D, 0x3F, 0x03, 0x31, 0x1F, 0x16, 0x92,
+		0x6D, 0x2D, 0x46, 0x41, 0x70, 0x2B, 0x79, 0x72
+	};
+
+	random_number = (unsigned char *)malloc(32);
+	memcpy(random_number, data, 32);
+
+	return random_number;
+}
+
+void print_number(size_t len, unsigned char *data) {
+	for (size_t i = 0; i < len; i++) {
+		printf("%02X ", data[i]);
+	}
+	printf("\n");
+}
+
+int main(int argc, char **argv) {
+	(void) argc; (void) argv;
+	//Obtain SN and key e.g. from DB server
+	unsigned char sn_data[9] = {
+		0x01, 0x23, 0x43, 0x39, 0xB5, 0x36, 0xA7, 0xC9, 0xEE
+	};
+
+	unsigned char key_data[32] = {
+		0xBE, 0xBA, 0x48, 0x40, 0xD6, 0x45, 0xDE, 0xE5,
+		0xA2, 0x37, 0x6F, 0x2F, 0x68, 0x35, 0x3B, 0xA6,
+		0xAA, 0x74, 0xE1, 0xA1, 0x3C, 0xE8, 0xB6, 0x99,
+		0x95, 0xAB, 0x31, 0x83, 0x1A, 0x01, 0xFB, 0x9A
+	};
+
+	//Init library
+	//Enable verbose mode is highly recommended
+	atsha_set_verbose();
+	atsha_set_log_callback(log_callback);
+
+	//Create new server-side emulation handler with obtained data
+	struct atsha_handle *handle = atsha_open_server_emulation(sn_data, key_data);
+	if (handle == NULL) {
+		//There is just one reason of error - memory allocation failed
+		fprintf(stderr, "Couldn't create server emulation handler.\n");
+		return 1;
+	}
+
+	//Check operations status
+	int status;
+	//Place for output
+	atsha_big_int response, sn;
+	response.data = sn.data = NULL;
+	//And some input
+	atsha_big_int challenge = {
+		.bytes = 32, //32byte input is required
+		.data = get_random_number()
+	};
+
+	//Do some funny stuff
+	printf("Serial number:\n");
+	status = atsha_serial_number(handle, &sn);
+	if (status != ATSHA_ERR_OK) {
+		printf("Operation failed: %s\n", atsha_error_name(status));
+	}
+	printf("SN is %u bytes long number:\n", (unsigned)sn.bytes);
+	print_number(sn.bytes, sn.data);
+
+	printf("Challenge response:\n");
+	status = atsha_challenge_response(handle, challenge, &response);
+	if (status != ATSHA_ERR_OK) {
+		printf("Operation failed: %s\n", atsha_error_name(status));
+	}
+	printf("Response is %u bytes long number:\n", (unsigned)response.bytes);
+	print_number(response.bytes, response.data);
+
+	//Free memory allocated by you
+	free(sn.data);
+	free(challenge.data);
+	free(response.data);
+
+	//Close all files, free allocated memory etc.
+	atsha_close(handle);
+
+	return 0;
+}
