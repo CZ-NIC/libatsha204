1882323991ee1ea2e96484d07662ea5d3e117dfa
Robin Obůrka <robin.oburka@nic.cz>
2013-08-13 10:09:40 +0200

    Debug printouts removed

--- src/libatsha204/layer_i2c.c
+++ src/libatsha204/layer_i2c.c
@@ -28,7 +28,6 @@ static int i2c_reset(struct atsha_handle *handle) {
 	unsigned char wr_addr[] = { ATSHA204_I2C_ADDRESS };
 	int status;
 
-log_message("RESET");
 	status = Start(handle->i2c);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_reset: Start");
@@ -37,17 +36,16 @@ log_message("RESET");
 
 	status = Write(handle->i2c, (char *)wr_addr, 1);
 	if (status != MPSSE_OK) {
-		log_message("layer_i2c: i2c_read: Write addr");
+		log_message("layer_i2c: i2c_reset: Write addr");
 		return ATSHA_ERR_COMMUNICATION;
 	}
-i2c_ack(handle);
 
 	status = Write(handle->i2c, (char *)wr_reset, 1);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_reset: Write");
 		return ATSHA_ERR_COMMUNICATION;
 	}
-i2c_ack(handle);
+
 	status = Stop(handle->i2c);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_reset: Stop");
@@ -64,8 +62,8 @@ static int i2c_read(struct atsha_handle *handle, unsigned char **answer) {
 	unsigned char *data;
 	int status;
 
-	wr_addr[0] |= 1;
-log_message("READ");
+	wr_addr[0] |= 0x01;
+
 	status = Start(handle->i2c);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_read: Start");
@@ -77,13 +75,13 @@ log_message("READ");
 		log_message("layer_i2c: i2c_read: Write addr");
 		return ATSHA_ERR_COMMUNICATION;
 	}
-i2c_ack(handle);
+
 	data = (unsigned char *)Read(handle->i2c, BUFFSIZE_I2C);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_read: Read");
 		return ATSHA_ERR_COMMUNICATION;
 	}
-i2c_ack(handle);
+
 	if (data == NULL) {
 		log_message("layer_i2c: i2c_command: No data read");
 		return ATSHA_ERR_COMMUNICATION;
@@ -109,7 +107,6 @@ int i2c_wake(struct atsha_handle *handle, unsigned char **answer) {
 	char wr_wake[] = { 0x00 };
 	int status;
 
-log_message("WAKE");
 	status = SetClock(handle->i2c, ATSHA204_I2C_WAKE_CLOCK);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_wake: SetClock - set frequency for wake");
@@ -147,13 +144,6 @@ log_message("WAKE");
 		return status;
 	}
 
-	i2c_wait();
-
-	status = i2c_reset(handle);
-	if (status != ATSHA_ERR_OK) {
-		return status;
-	}
-
 	return ATSHA_ERR_OK;
 }
 
@@ -162,7 +152,6 @@ int i2c_command(struct atsha_handle *handle, unsigned char *raw_packet, unsigned
 	unsigned char wr_cmd[] = { ATSHA204_I2C_WA_COMMAND };
 	int status;
 
-log_message("COMMAND");
 	status = Start(handle->i2c);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_command: Start");
@@ -174,20 +163,19 @@ log_message("COMMAND");
 		log_message("layer_i2c: i2c_command: Write addr");
 		return ATSHA_ERR_COMMUNICATION;
 	}
-i2c_ack(handle);
-status = Write(handle->i2c, (char *)wr_cmd, 1);
+
+	status = Write(handle->i2c, (char *)wr_cmd, 1);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_command: Write addr");
 		return ATSHA_ERR_COMMUNICATION;
 	}
-print_buffer_content(raw_packet, raw_packet[0]);
-i2c_ack(handle);
+
 	status = Write(handle->i2c, (char *)raw_packet, raw_packet[0]);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_command: Write packet");
 		return ATSHA_ERR_COMMUNICATION;
 	}
-i2c_ack(handle);
+
 	status = Stop(handle->i2c);
 	if (status != MPSSE_OK) {
 		log_message("layer_i2c: i2c_command: Stop");
@@ -201,12 +189,101 @@ i2c_ack(handle);
 		return status;
 	}
 
-	i2c_wait();
-
-	status = i2c_reset(handle);
-	if (status != ATSHA_ERR_OK) {
-		return status;
-	}
-
 	return ATSHA_ERR_OK;
 }
+
+//Code bellow this point is muster of communication
+#if 0
+int maiXYZ(void) {
+	char *data = NULL;
+	size_t data_len = 128;
+	struct mpsse_context *i2c = NULL;
+	int ack;
+	int status;
+	/*char wrdata[] = {
+		0x03,
+		0x07,
+		0x30,
+		0x00,
+		0x00,
+		0x00,
+		0x03,
+		0x5d
+	 };*/
+
+	 char wrdata[] = {
+		0x03,
+		0x07,
+		0x1B,
+		0x00,
+		0x00,
+		0x00,
+		0x24,
+		0xcd
+	 };
+
+	char wrtransmit[] = {
+		0x88
+	};
+
+	char wrzero[] = {
+		0x00
+	};
+
+	 char wraddr[] = {
+		ADDR
+	};
+printf("OK: %d\n", MPSSE_OK);
+	i2c = MPSSE(I2C, 10000, MSB); //# Initialize libmpsse for I2C operations at 400kHz
+	if (i2c == NULL) return 1;
+
+
+	status = Start(i2c); printf("Status: %d\n", status);
+	SendAcks(i2c);
+
+	status = Write(i2c, wrzero, 1); printf("Status: %d\n", status);
+	printf("ACK: %d\n", GetAck(i2c));
+	status = Stop(i2c); printf("Status: %d\n", status);
+	usleep(10000);
+
+	status = Start(i2c);
+	status = Write(i2c, wraddr, 1); printf("Status: %d\n", status);
+	printf("ACK: %d\n", GetAck(i2c));
+
+	status = Write(i2c, wrdata, 8); printf("Status: %d\n", status);
+	printf("ACK: %d\n", GetAck(i2c));
+	status = Stop(i2c); printf("Status: %d\n", status);
+	usleep(10000);
+
+	status = Start(i2c);
+	wraddr[0] |= 1;
+	status = Write(i2c, wraddr, 1); printf("Status: %d\n", status);
+	printf("ACK: %d\n", GetAck(i2c));
+	data = Read(i2c, 10); printf("Status: %d\n", status);
+
+	//status = Write(i2c, wrtransmit, 1); printf("Status: %d\n", status);
+	//printf("ACK: %d\n", GetAck(i2c));
+
+//	data = Read(i2c, 1); printf("Status: %d\n", status);
+//	Read(i2c, 1); //Read one last "dummy" byte from the I2C slave in order to send the NACK
+
+
+	Stop(i2c); //Send I2C stop condition
+	Close(i2c); //Deinitialize libmpsse
+
+	if (data == NULL) {
+		printf("No data\n");
+	} else {
+		size_t i;
+		for (i = 0; i < 40; i++) {
+			if ((i % 8) == 0) printf("\n");
+			printf("0x%02X ", data[i]);
+		}
+		printf("\n");
+	}
+
+	free(data);
+	return 0;
+}
+
+#endif
