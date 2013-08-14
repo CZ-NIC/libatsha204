9cea51dd475acc212c9d878529c248b11c4c3050
Robin Obůrka <robin.oburka@nic.cz>
2013-08-14 13:52:35 +0200

    [chipinit] Chip initialization is completed.

--- src/chipinit/main.c
+++ src/chipinit/main.c
@@ -27,7 +27,6 @@ static void dump_config(struct atsha_handle *handle) {
 
 	printf("Config zone (0x00 - 0x15):\n");
 	for (unsigned char addr = 0x00; addr <= 0x15; addr++) {
-		sleep(1);
 		atsha_raw_conf_read(handle, addr, &data);
 		printf("0x%02X: ", addr);
 		for (size_t i = 0; i < data.bytes; i++) {
@@ -43,7 +42,6 @@ static void dump_data(struct atsha_handle *handle) {
 
 	printf("Data zone (slot 0 - 15):\n");
 	for (unsigned char slot = 0; slot <= 15; slot++) {
-		sleep(1);
 		atsha_raw_slot_read(handle, slot, &data);
 		printf("%2u: ", slot);
 		for (size_t i = 0; i < data.bytes; i++) {
@@ -58,7 +56,6 @@ static void dump_otp(struct atsha_handle *handle) {
 
 	printf("OTP zone (0x00 - 0x0F):\n");
 	for (unsigned char addr = 0x00; addr <= 0x0F; addr++) {
-		sleep(1);
 		atsha_raw_otp_read(handle, addr, &data);
 		printf("0x%02X: ", addr);
 		for (size_t i = 0; i < data.bytes; i++) {
@@ -111,6 +108,7 @@ static bool set_otp_mode(struct atsha_handle *handle, unsigned char *config) {
 	return true;
 
 }
+
 static bool set_slot_config(struct atsha_handle *handle, unsigned char *config) {
 	atsha_big_int record;
 
@@ -155,6 +153,37 @@ static bool create_and_lock_config(struct atsha_handle *handle) {
 	return true;
 }
 
+static bool write_and_lock_data(struct atsha_handle *handle, unsigned char *data, unsigned char *otp) {
+	atsha_big_int number;
+
+	//Write keys into chip
+	number.bytes = BYTESIZE_KEY;
+	for (size_t item = 0; item < SLOT_CNT; item++) {
+		memcpy(number.data, (data + (item * BYTESIZE_KEY)), number.bytes);
+		if (atsha_raw_slot_write(handle, item, number) != ATSHA_ERR_OK) return false;
+	}
+
+	//Write OTP items into chip
+	size_t item = 0;
+	number.bytes = BYTESIZE_OTP;
+	for (unsigned char addr = 0x00; addr <= 0x0F; addr++) {
+		memcpy(number.data, (otp + (item * BYTESIZE_OTP)), number.bytes);
+		item++;
+		if (atsha_raw_otp_write(handle, addr, number) != ATSHA_ERR_OK) return false;
+	}
+
+	unsigned char crc[2];
+	unsigned char both[(SLOT_CNT*BYTESIZE_KEY)+(SLOT_CNT*BYTESIZE_OTP)];
+
+	memcpy(both, data, (SLOT_CNT*BYTESIZE_KEY));
+	memcpy((both+(SLOT_CNT*BYTESIZE_KEY)), otp, (SLOT_CNT*BYTESIZE_OTP));
+
+	calculate_crc((SLOT_CNT*BYTESIZE_KEY)+(SLOT_CNT*BYTESIZE_OTP), both, crc);
+	if (atsha_lock_data(handle, crc) != ATSHA_ERR_OK) return false;
+
+	return true;
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s config\n", argv[0]);
@@ -169,25 +198,29 @@ int main(int argc, char **argv) {
 	//init LIBATSHA204
 	atsha_set_verbose();
 	atsha_set_log_callback(log_callback);
+
 	//Create LIBATSHA204 handler
 	struct atsha_handle *handle = atsha_open_i2c_dev();
 	if (handle == NULL) {
 		fprintf(stderr, "Couldn't open I2C devidce.\n");
 		return 1;
 	}
-
+/*
+	dump_config(handle);
+	dump_otp(handle);
+	dump_data(handle);
+	return 0;
+//*/
 	if (create_and_lock_config(handle)) {
 		printf("Configuration is locked\n");
 	} else {
 		printf("Configuration is NOT locked\n");
 	}
-	dump_config(handle);
-	//dump_config(handle);
-return 0;
+
 	//Prepare data structures
 	unsigned char data[SLOT_CNT*BYTESIZE_KEY];
 	unsigned char otp[SLOT_CNT*BYTESIZE_OTP];
-	atsha_big_int number;
+
 	//Read keys
 	if (!read_config(conf, data, BYTESIZE_KEY)) {
 		fprintf(stderr, "Couldn't read config data (keys).\n");
@@ -198,28 +231,17 @@ return 0;
 		fprintf(stderr, "Couldn't read config data (OTP).\n");
 		return 1;
 	}
-	//Calculate CRC of zones
-	unsigned char crc_data[2], crc_otp[2];
-	calculate_crc((unsigned char)(SLOT_CNT*BYTESIZE_KEY), data, crc_data);
-	calculate_crc((unsigned char)(SLOT_CNT*BYTESIZE_OTP), otp, crc_otp);
-	//Write keys into chip
-	number.bytes = BYTESIZE_KEY;
-	for (size_t item = 0; item < SLOT_CNT; item++) {
-		memcpy(number.data, (data + (item * BYTESIZE_KEY)), number.bytes);
-		atsha_raw_slot_write(handle, item, number);
-		sleep(1);
-	}
-	//Write OTP items into chip
-	size_t item = 0;
-	number.bytes = BYTESIZE_OTP;
-	for (unsigned char addr = 0x00; addr <= 0x0F; addr++) {
-		memcpy(number.data, (otp + (item * BYTESIZE_OTP)), number.bytes);
-		item++;
-		atsha_raw_otp_write(handle, addr, number);
-		sleep(1);
+
+	//Write data
+	if (write_and_lock_data(handle, data, otp)) {
+		printf("Data and OTP zones are locked\n");
+	} else {
+		printf("Data and OTP zones are NOT locked\n");
 	}
 
-//	dump_device(handle);
+	dump_config(handle);
+	dump_otp(handle);
+
 	fclose(conf);
 	atsha_close(handle);
 
