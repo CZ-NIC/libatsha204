105f7a38d3006047c51972f9827d44fb6cc8550c
Stepan Henek <stepan.henek@nic.cz>
2013-08-27 11:25:43 +0200

    Python wrapper: function which calculates hmac using chip added + asserts removed + docstrings added

--- src/python/atsha204.c
+++ src/python/atsha204.c
@@ -5,32 +5,90 @@
 /*
  * As linking to other libraries from python extensions is tricky,
  * we do fully dynamic loading of the library with dlopen.
- *
- * Also, most of the error checking is done by assert instead of
- * python exceptions, out of laziness.
  */
 
-static PyObject *atsha_do_hmac(PyObject *self, PyObject *args) {
+static PyObject *atsha_emulate_hmac(PyObject *self, PyObject *args) {
 	(void) self;
 	int size_serial, size_key, size_challenge;
 	unsigned char slot_id;
 	const uint8_t *serial, *key, *challenge;
-	if (!PyArg_ParseTuple(args, "bs#s#s#", &slot_id, &serial, &size_serial, &key, &size_key, &challenge, &size_challenge))
+	if (!PyArg_ParseTuple(args, "bs#s#s#", &slot_id, &serial, &size_serial, &key, &size_key, &challenge, &size_challenge)) {
 		return NULL;
-	assert(size_key == 32);
-	assert(size_challenge == 32);
+	}
+
+	if (32 != size_key) {
+		PyErr_SetString(PyExc_ValueError, "key length should be 32!");
+		return NULL;
+	}
+	if (32 != size_challenge) {
+		PyErr_SetString(PyExc_ValueError, "challenge length should be 32!");
+		return NULL;
+	}
+
 	struct atsha_handle *crypto = atsha_open_server_emulation(slot_id, serial, key);
+	if (NULL == crypto) {
+        PyErr_SetString(PyExc_RuntimeError, "failed to initialize crypto library");
+        return NULL;
+    }
 	atsha_big_int challenge_s, response_s;
 	challenge_s.bytes = 32;
 	memcpy(challenge_s.data, challenge, 32);
 	int result = atsha_challenge_response(crypto, challenge_s, &response_s);
-	assert(result == ATSHA_ERR_OK);
+	if (ATSHA_ERR_OK != result) {
+		PyErr_SetString(PyExc_ValueError, atsha_error_name(result));
+		return NULL;
+	}
+	atsha_close(crypto);
+	return Py_BuildValue("s#", response_s.data, (int) response_s.bytes);
+}
+
+static PyObject *atsha_do_hmac(PyObject *self, PyObject *args) {
+	(void) self;
+	int size_challenge;
+	const uint8_t *challenge;
+	if (!PyArg_ParseTuple(args, "s#", &challenge, &size_challenge)) {
+		return NULL;
+	}
+
+	if (32 != size_challenge) {
+		PyErr_SetString(PyExc_ValueError, "challenge length should be 32!");
+		return NULL;
+	}
+
+	struct atsha_handle *crypto = atsha_open();
+	if (NULL == crypto) {
+        PyErr_SetString(PyExc_RuntimeError, "failed to initialize crypto library");
+        return NULL;
+    }
+	atsha_big_int challenge_s, response_s;
+	challenge_s.bytes = 32;
+	memcpy(challenge_s.data, challenge, 32);
+	int result = atsha_challenge_response(crypto, challenge_s, &response_s);
+	if (ATSHA_ERR_OK != result) {
+		PyErr_SetString(PyExc_ValueError, atsha_error_name(result));
+		return NULL;
+	}
 	atsha_close(crypto);
 	return Py_BuildValue("s#", response_s.data, (int) response_s.bytes);
 }
 
 static PyMethodDef atsha_methods[] = {
-	{"hmac", atsha_do_hmac, METH_VARARGS, NULL},
+	{
+		"emulate_hmac",
+		atsha_emulate_hmac,
+		METH_VARARGS,
+		"Emulates computation of hmac from atsh204 cryptographic chip.\n"
+		"hmac(slot_id, serial, key, challenge) where len(key)=32 and len(challenge)=32"
+	},
+	{
+		"hmac",
+		atsha_do_hmac,
+		METH_VARARGS,
+		"Computes a hmac using atsh204 cryptographic chip.\n"
+		"hmac(challenge) where len(challenge)=32\n"
+		"\n"
+		"Note than internet connection is required to obtain a slot_id from DNS"
+	},
 	{NULL}
 };
 
