36646b6bb8fa5ac9b73c5322259b953450d75bf6
Stepan Henek <stepan.henek@nic.cz>
2013-08-28 12:56:47 +0200

    libatsha204.so contains library dependencies crypto and unbound + dependencies crypto and unbound removed from python wrapper.

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 
-extension = Extension('atsha204', ['atsha204.c'], include_dirs=['../libatsha204'], library_dirs=['../libatsha204'], libraries=['crypto', 'ssl', 'unbound', 'atsha204'])
+extension = Extension('atsha204', ['atsha204.c'], include_dirs=['../libatsha204'], library_dirs=['../libatsha204'], libraries=['atsha204'])
 setup(name='atsha204', version='0.1', ext_modules=[extension])
