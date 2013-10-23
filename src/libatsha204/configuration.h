36febc424d3077b64ec87f5809fb7faeb9964475
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-10-23 09:50:20 +0200

    Change the location of slot TXT record

--- src/libatsha204/configuration.h
+++ src/libatsha204/configuration.h
@@ -20,7 +20,7 @@
 #define DEFAULT_USB_DEV_PATH "/dev/hidraw0"
 #define DEFAULT_NI2C_DEV_PATH "/dev/i2c-0"
 #define DEFAULT_USE_SN_IN_DIGEST true
-#define DEFAULT_DNS_RECORD_FIND_KEY "use_key.extremehost.cz"
+#define DEFAULT_DNS_RECORD_FIND_KEY "atsha-key.turris.cz"
 #define DEFAULT_DNSSEC_ROOT_KEY "/etc/dnssec-root.key"
 #define LOCK_FILE "/tmp/libatsha204.lock"
 #define LOCK_TRY_TOUT 10000
