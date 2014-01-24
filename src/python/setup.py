4dab32fa345c546c038dce80582d6644c11a2325
Stepan Henek <stepan.henek@nic.cz>
2014-01-24 08:27:41 +0100

    python: 'provides' added to setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -18,4 +18,4 @@
 from distutils.core import setup, Extension
 
 extension = Extension('atsha204', ['atsha204.c'], libraries=['atsha204'])
-setup(name='atsha204', version='0.1', ext_modules=[extension])
+setup(name='atsha204', version='0.1', ext_modules=[extension], provides=['atsha204'])
