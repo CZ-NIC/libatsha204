aed9ade7076821ecf969e73ca41ceeca67f67c3a
Robin Obůrka <robin.oburka@nic.cz>
2016-01-29 16:17:32 +0100

    chiptools: Provide argument parser wit option to set custom bus and address

--- src/chiptools/main.c
+++ src/chiptools/main.c
@@ -22,16 +22,67 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
+#include <argp.h>
+#include <stdlib.h>
 
 #include "../libatsha204/atsha204.h"
 
 #include "commands.h"
 
+struct arguments {
+	bool use_default;
+	const char* path;
+	int address;
+	int commands_from;
+};
 
 void log_callback(const char *msg) {
 	fprintf(stderr, "Log: %s\n", msg);
 }
 
+static char doc[] = "ATSHA204(A) manipulation tool";
+static char args_doc[] = "COMMAND";
+static struct argp_option options[] = {
+	{"default",				'd', 0, 0, "Use default values for bus and address", 0},
+	{"address",				'a', "ADDR", 0, "Set address of ATSHA204(A)", 0},
+	{"path",				'p', "STR", 0, "Set path of I2C BUS", 0},
+	{ 0, 0, 0, 0, 0, 0 }
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	char *endptr;
+	struct arguments *args = state->input;
+	switch (key) {
+	case 'd':
+		args->use_default = true;
+		break;
+	case 'a':
+		endptr = arg;
+		args->address = strtol(arg, &endptr, 0);
+		args->use_default = false;
+		break;
+	case 'p':
+		args->path = arg;
+		args->use_default = false;
+		break;
+	case ARGP_KEY_NO_ARGS:
+		argp_usage(state);
+		break;
+	case ARGP_KEY_ARG:
+		if (args->commands_from == 0) {
+			args->commands_from = state->next - 1;
+			state->next = state->argc;
+		}
+		break;
+	//case ARGP_KEY_END:
+	//	break;
+	}
+
+	return 0;
+}
+
+static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
 
 static void print_abi(atsha_big_int abi) {
 	for (size_t i = 0; i < abi.bytes; i++) {
@@ -41,24 +92,36 @@ static void print_abi(atsha_big_int abi) {
 }
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s command\n", argv[0]);
-		return 1;
-	}
+	struct arguments args = (struct arguments) {
+		.use_default = true,
+		.address = 0,
+		.path = NULL,
+		.commands_from = 0
+	};
 
-	atsha_big_int abi;
+	// Parse arguments
+	argp_parse(&argp, argc, argv, 0, 0, &args);
+	int cmdi = args.commands_from;
+	const char *cmd = argv[cmdi];
 
 	//init LIBATSHA204
 	atsha_set_verbose();
 	atsha_set_log_callback(log_callback);
 
 	//Create LIBATSHA204 handler
-	struct atsha_handle *handle = atsha_open();
+	struct atsha_handle *handle = NULL;
+	if (args.use_default) {
+		handle = atsha_open();
+	} else {
+		handle = atsha_open_ni2c_dev(args.path, args.address);
+	}
 	if (handle == NULL) {
 		fprintf(stderr, "Couldn't open I2C devidce.\n");
 		return 1;
 	}
-	char *cmd = argv[1];
+
+	atsha_big_int abi;
+
 	if (strcmp(cmd, "dump-config") == 0) {
 		dump_config(handle);
 
